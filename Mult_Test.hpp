63832ab7d88f3a2744cc6a758b187151647daa92
AlliyahMunir <52710942+AlliyahMunir@users.noreply.github.com>
2020-11-03 14:45:16 -0800

    Update Mult_Test.hpp

--- Mult_Test.hpp
+++ Mult_Test.hpp
@@ -11,7 +11,19 @@ TEST(MultTest, MultEvalTwoPos) {
     Mult* finaltest = new Mult(test1,test2);
    // test2 = test2final-> evaluate();
     EXPECT_EQ(finaltest->evaluate(),81);
+    EXPECT_EQ(test->stringify(), "9.0000000*9.000000");
 
-};
+}
+
+TEST(MultTest, MultEvalTwoPos) {
+    Base* test1 = new Op(2);
+    Base* test2 = new Op(2);
+    Mult* finaltest = new Mult(test1,test2);
+   // test2 = test2final-> evaluate();
+    EXPECT_EQ(finaltest->evaluate(),4);
+        EXPECT_EQ(test->stringify(), "2.000000*2.000000");
+
+}
+
 
 #endif //__MULT_TEST_HPP__
