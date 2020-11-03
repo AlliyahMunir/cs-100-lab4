d0bf4fee70124ef8f9fd51edfa689b090e407568
AlliyahMunir <52710942+AlliyahMunir@users.noreply.github.com>
2020-11-03 14:41:26 -0800

    Update Rand_Test.hpp

--- Rand_Test.hpp
+++ Rand_Test.hpp
@@ -9,5 +9,14 @@ TEST(RandTest, RandEvalPos) {
     Rand* test = new Rand(67);
    EXPECT_NEAR(test->evaluate(),0, 100);
        };
+
+TEST(RandTest, RandEvalPos) {
+    Rand* test = new Rand(45);
+   EXPECT_NEAR(test->evaluate(),0, 100);
+       };
+TEST(RandTest, RandEvalPos) {
+    Rand* test = new Rand(13);
+   EXPECT_NEAR(test->evaluate(),0, 100);
+       };
    
      #endif //__RAND_TEST_HPP__
